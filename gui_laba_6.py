fb60d6236eca94b0e16735f76881704f1b963e85
galvains <93620542+galvains@users.noreply.github.com>
2023-03-22 00:36:09 +0300

    worked

--- gui_laba_6.py
+++ gui_laba_6.py
@@ -1,27 +1,56 @@
+import json
 from tkinter import *
 from tkinter.messagebox import showinfo
+
+
+def create_json():
+    with open('data.json', 'w') as file:
+
+        data = {}
+        data['users'] = []
+        json.dump(data, file, indent=4, ensure_ascii=False)
 
 
 def confirm():
 
     def get_info():
 
+        if ent_repeat.get() == ent_pass.get():
+            if showinfo(message='Connection is successful!'):
+                window.destroy()
 
-        # counter = 2
-        # while counter > 0:
-        #     if ent_conf.get() == ent_pass.get():
-        #         showinfo(message='Welcome!')
-        #         window.quit()
-        #         break
-        #     else:
-        #         lbl_count['text'] = f'Left to try: {counter}'
-        #
-        #     counter -= 1
+        try:
+            with open('data.json') as file:
+
+                data = json.load(file)
+                name = ent_name.get()
+                password = ent_pass.get()
+
+                new_user = {'username': name, 'password': password}
+                data['users'].append(new_user)
+
+                with open('data.json', 'w') as outfile:
+                    json.dump(data, outfile,  indent=4, ensure_ascii=False)
+
+        except FileNotFoundError:
+            print('[ERROR] json-file not found')
+
+        else:
+            count = int(lbl_count['text'][-1])
+            if count > 1:
+                count -= 1
+                lbl_count['text'] = f'Left to try: {str(count)}'
+            else:
+                if showinfo(message='Connection closed.'):
+                    window.destroy()
 
 
-    if ent_name.get() and ent_pass.get():
+    try:
+        if not ent_name.get() and not ent_pass.get():
+            raise KeyError
+
         window = Tk()
-        window.title('confirm')
+        window.title('Repeat')
         window.resizable(width=False, height=False)
 
         x = (window.winfo_screenwidth() - window.winfo_reqwidth()) / 2 + 20
@@ -31,40 +60,42 @@ def confirm():
         frame = Frame(window)
         frame.pack()
 
-        lbl_conf = Label(frame, text='Repeat password:')
-        lbl_conf.grid(column=0, row=0, sticky='w')
+        lbl_repeat = Label(frame, text='Repeat password:')
+        lbl_repeat.grid(column=0, row=0, sticky='w')
 
-        ent_conf = Entry(frame, show='*')
-        ent_conf.grid(column=0, row=1)
+        ent_repeat = Entry(frame, show='*')
+        ent_repeat.grid(column=0, row=1)
 
-        lbl_count = Label(frame)
+        lbl_count = Label(frame, text='Left to try: 3')
         lbl_count.grid(column=0, row=2, sticky='w', padx=10)
 
-        btn_okk = Button(frame, text='Ok', command=get_info)
-        btn_okk.grid(column=0, row=2, sticky='e', padx=10, pady=4)
+        btn_repeat = Button(frame, text='Ok', command=get_info)
+        btn_repeat.grid(column=0, row=2, sticky='e', padx=10, pady=4)
 
+        window.protocol("WM_DELETE_WINDOW", get_info)
         window.mainloop()
-    else:
-        showinfo(message='Login or password error')
+
+    except KeyError:
+        showinfo(message='Login or pass error...')
+
 
 window = Tk()
 window.title('sign in')
 window.resizable(width=False, height=False)
+# window.geometry('250x150')
 
 x = (window.winfo_screenwidth() - window.winfo_reqwidth()) / 2
 y = (window.winfo_screenheight() - window.winfo_reqheight()) / 2
 window.wm_geometry("+%d+%d" % (x, y))
 
-window.rowconfigure([0, 1, 2, 3, 4], minsize=1)
-
 frame_pass = Frame(window, borderwidth=2, relief=SUNKEN)
 frame_btn = Frame(window)
 
-frame_pass.pack(side=TOP)
+frame_pass.pack(side=TOP, fill=BOTH)
 frame_btn.pack(side=BOTTOM)
 
-lbl_name = Label(frame_pass, text='Enter name:')
-lbl_pass = Label(frame_pass, text='Enter pass:')
+lbl_name = Label(frame_pass, text='name:')
+lbl_pass = Label(frame_pass, text='pass:')
 
 lbl_name.grid(column=0, row=0, sticky='w')
 lbl_pass.grid(column=0, row=2, sticky='w')
@@ -72,13 +103,15 @@ lbl_pass.grid(column=0, row=2, sticky='w')
 ent_name = Entry(frame_pass)
 ent_pass = Entry(frame_pass, show='*')
 
-ent_name.grid(column=0, row=1)
-ent_pass.grid(column=0, row=3)
+ent_name.grid(column=1, row=0, sticky='e')
+ent_pass.grid(column=1, row=2, sticky='e')
 
 btn_ok = Button(frame_btn, text='Ok', command=confirm)
-btn_cancel = Button(frame_btn, text='Cancel', command=exit)
+btn_cancel = Button(frame_btn, text='Cancel', command=window.destroy)
+btn_create_json = Button(frame_btn, text='Create json', command=create_json)
 
-btn_ok.grid(row=4, column=0, pady=3)
-btn_cancel.grid(row=4, column=1)
+btn_ok.grid(row=4, column=0, pady=4)
+btn_cancel.grid(row=4, column=1, padx=3)
+btn_create_json.grid(row=4, column=2)
 
 window.mainloop()
