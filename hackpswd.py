8feb4a0269607d1a41518597d50bd2024dc5af09
galvains <93620542+galvains@users.noreply.github.com>
2023-03-22 17:54:07 +0300

    lab_6

--- hackpswd.py
+++ hackpswd.py
@@ -1,3 +1,4 @@
+import json
 from tkinter import *
 from tkinter.messagebox import showinfo
 
@@ -16,6 +17,23 @@ def brute_force():
     except KeyError:
         showinfo(message='Sorry, my dictionary is too stupid for that password 😕')
         # window.destroy()
+
+def dict_hack():
+    try:
+        with open('data.json') as file:
+            data = json.load(file)
+
+            # if ent_pass.get() in data['users']['password']:
+            #     result['text'] = data['users']['password']
+        for user in data['users']:
+            if user['password'] == ent_pass.get():
+                result['text'] = user['password']
+                break
+        else:
+            raise KeyError
+
+    except KeyError:
+        showinfo(message='Sorry, my dictionary is too stupid for that password 😕')
 
 
 window = Tk()
@@ -46,7 +64,10 @@ result = Label(frame,)
 result.grid(row=2, column=1, sticky='w', pady=2)
 
 btn_force = Button(frame_btn, text='brute force', command=brute_force)
+btn_dict = Button(frame_btn, text='dict', command=dict_hack)
+
 btn_force.grid(row=3, column=0, pady=3)
+btn_dict.grid(row=3, column=1, pady=3)
 
 window.mainloop()
 
